4a1262aa07010ad85fa9e670baf2b00a68ebf11c
Ralphie Celedon <ralphie94@pacbell.net>
2023-02-11 19:11:05 -0800

    setup project, started navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,15 @@
+import styles from "../styles/Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.item}>
+
+        </div>
+        <div className={styles.item}></div>
+        <div className={styles.item}></div>
+    </div>
+  )
+}
+
+export default Navbar;
