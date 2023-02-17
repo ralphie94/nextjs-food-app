04342ee3fec7b10f27fe0fda47eaf98e6db91534
Ralphie Celedon <ralphie94@pacbell.net>
2023-02-16 23:54:50 -0800

    building out featured main page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import Featured from "../components/Featured";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
         <meta name="description" content="Best pizza in" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      homepage
+      <Featured />
     </div>
   )
 }
